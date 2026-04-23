d84bc47c40730779f0c0c0c01ac0d3c9fc813bb7
thachv04012 <thachvu04012@gmail.com>
2026-04-23 13:37:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,13 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base:'/tiksave-download-2.0/',
+    server:
+      
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
+    ,
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
